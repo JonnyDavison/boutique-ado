9fa5cbc0f4cd680558e505d3f319f9aacb60d02e
JonnyDavison <jonnydavison@icloud.com>
2023-07-03 13:28:40 +0000

    Add webhook listener

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -14,3 +14,19 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
+    
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from strip
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+            """
+            Handle the payment_intent.payment_failed webhook from stripe
+            """
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]}',
+                status=200)
