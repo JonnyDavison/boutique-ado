a85913ae0809ea6c43cd9aeccbf7977f1c393c76
JonnyDavison <jonnydavison@icloud.com>
2023-07-05 17:22:26 +0000

    Update order of Email settings in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+ 
 ALLOWED_HOSTS = ['ecom-boutique-ado-fed8d736c954.herokuapp.com', '8000-jonnydaviso-boutiqueado-km4296tfuxq.ws-eu101.gitpod.io']
 
 
@@ -216,8 +216,8 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_PORT = 587
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
