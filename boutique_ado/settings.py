3f57b8630ece5f818376365d046fc7b506a92b12
JonnyDavison <jonnydavison@icloud.com>
2023-07-05 18:18:30 +0000

    Update debug status

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
  
 ALLOWED_HOSTS = ['ecom-boutique-ado-fed8d736c954.herokuapp.com', '8000-jonnydaviso-boutiqueado-km4296tfuxq.ws-eu101.gitpod.io']
 
